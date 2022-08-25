91e84cc065293cae4f9b90a3c01d6bfaeaca832c
Ahmed Ibrahim <ib153507@gmail.com>
2022-08-25 07:44:29 +0200

    Copy input image to the pixels buffer

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,16 @@
-use clap::{Arg, Parser};
+use clap::Parser;
+use image::ImageError;
+use pixels::{Pixels, SurfaceTexture};
 use thiserror::Error;
 use winit::{
+    dpi::PhysicalSize,
     error::OsError,
     event::{Event, KeyboardInput, WindowEvent},
     event_loop::EventLoop,
     window::{CursorIcon, WindowBuilder},
 };
+
+const SCREEN_PERCENT: u32 = 90;
 
 /// Simple program to view images
 #[derive(Parser, Debug)]
@@ -20,15 +25,61 @@ struct Args {
 enum ErrorWrapper {
     #[error("Unable to create the main window")]
     WindowError(#[from] OsError),
+    #[error("Unable to load the image")]
+    IoError(#[from] std::io::Error),
+    #[error("Unable to decode image")]
+    ImageError(#[from] ImageError),
+    #[error("Unable to calculate maximum screen size on your primary monitor")]
+    NoPrimaryMonitor,
+    #[error("Unable to view image pixels")]
+    PixelsError(#[from] pixels::Error),
 }
 
 fn main() -> Result<(), ErrorWrapper> {
     let args = Args::parse();
     // Load the image
+    let mut img = image::io::Reader::open(&args.image)?.decode()?;
+
     let event_loop = EventLoop::new();
+
+    let primary_monitor = event_loop
+        .primary_monitor()
+        .ok_or(ErrorWrapper::NoPrimaryMonitor)?;
+
+    let screen_size = primary_monitor.size();
+
+    let max_screen_size = (
+        screen_size.width * SCREEN_PERCENT / 100,
+        screen_size.height * SCREEN_PERCENT / 100,
+    );
+
+    let horz_scale = calc_scale(max_screen_size.0, img.width());
+    let vert_scale = calc_scale(max_screen_size.1, img.height());
+    let scale = horz_scale.max(vert_scale);
+    let window_inner_size = PhysicalSize::new(img.width() / scale, img.height() / scale);
+
     let window = WindowBuilder::new()
         .with_title("Img Viewer")
+        .with_inner_size(window_inner_size)
         .build(&event_loop)?;
+
+    let surface = SurfaceTexture::new(window_inner_size.width, window_inner_size.height, &window);
+    let mut pixels = Pixels::new(img.width(), img.height(), surface)?;
+
+    let img_bytes = img.as_mut_rgba8().unwrap().as_flat_samples();
+    let img_bytes = img_bytes.as_slice();
+
+    let pixels_bytes = pixels.get_frame();
+
+    img_bytes
+        .chunks_exact(4)
+        .zip(pixels_bytes.chunks_exact_mut(4))
+        .for_each(|(img, pixel)| {
+            pixel[0] = img[0];
+            pixel[1] = img[1];
+            pixel[2] = img[2];
+            pixel[3] = img[3];
+        });
 
     event_loop.run(move |event, _, control_flow| {
         control_flow.set_wait();
@@ -58,8 +109,18 @@ fn main() -> Result<(), ErrorWrapper> {
                 }
                 _ => {}
             },
-            Event::RedrawRequested(_) => {}
+            Event::RedrawRequested(_) => {
+                let _ = pixels.render();
+            }
             _ => {}
         }
     });
+}
+
+fn calc_scale(max_size: u32, curr_size: u32) -> u32 {
+    if max_size >= curr_size {
+        1
+    } else {
+        (curr_size as f32 / max_size as f32).ceil() as u32
+    }
 }
